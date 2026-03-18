d1ad2bbc724d7f65e6a55c117256d9c218f290aa
mrhappyaswq <158987103+mrhappyaswq@users.noreply.github.com>
2026-03-18 13:51:33 -0700

    Refactor carousel image navigation and update dots

--- script.js
+++ script.js
@@ -12,13 +12,17 @@ function showImage(index) {
 }
 
 prevBtn.addEventListener('click', () => {
+  // Left arrow should go **back**
   current = (current - 1 + images.length) % images.length;
   showImage(current);
+  updateDots();
 });
 
 nextBtn.addEventListener('click', () => {
+  // Right arrow should go **forward**
   current = (current + 1) % images.length;
   showImage(current);
+  updateDots();
 });
 
 // Initialize
