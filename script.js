ef18f4f503478cf7b4c72191bdfd643371011e03
mrhappyaswq <158987103+mrhappyaswq@users.noreply.github.com>
2026-03-18 14:00:34 -0700

    Refactor carousel image functionality and footer year

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function showImage(index) {
   images[index].classList.add('active');
 }
 
-prevBtn.addEventListener('click', () => {
+nextBtn.addEventListener('click', () => {
   // Left arrow should go **back**
   current = (current - 1 + images.length) % images.length;
   showImage(current);
