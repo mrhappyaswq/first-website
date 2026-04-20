eede1acac93e170e404767fd58de5ff778f492a0
mrhappyaswq <158987103+mrhappyaswq@users.noreply.github.com>
2026-04-20 11:47:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -79,13 +79,13 @@ document.querySelectorAll('.carousel').forEach(carousel => {
 
       // Use actual distance and angle for correct rotation on both axes
       const distance = Math.sqrt(dx * dx + dy * dy);
-      const maxDist = 400;
+      const maxDist = 800;
       const strength = Math.min(distance / maxDist, 1) * 25;
 
       // const maxDist = 400;
       
-      const rotateY = (dx / maxDist) * 25;   // left/right → Y axis
-      const rotateX = -(dy / maxDist) * 25;  // up/down → X axis
+      const rotateX = (dx / maxDist) * 10;   // left/right → Y axis
+      const rotateY = -(dy / maxDist) * 10;  // up/down → X axis
 
       activeImg.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1)`;
     }
